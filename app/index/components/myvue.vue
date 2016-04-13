a4078143d1a5fd8eaeb23e0d59817801aa54ea99
guo_hui <sdghld@163.com>
2016-04-13 17:35:11 +0800

    'app\index\components\myvue.vue'

---
+++ app/index/components/myvue.vue
@@ -0,0 +1,19 @@
+<template>
+    <div v-for="n in 20">eetttt</div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                msg: 'hello monkey!'
+            }
+        }
+    }
+</script>
+
+<style>
+    html{
+        background: red
+    }
+</style>
